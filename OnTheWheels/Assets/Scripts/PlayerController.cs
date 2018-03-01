5fe1e44134269a8873be9df0765362a8cd251492
Bruno Santos <bruno.santos1996@gmail.com>
2018-03-01 02:51:46 +0000

    started terrain collision

---
+++ OnTheWheels/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2d;
+
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update()
+	{
+		Vector2 moveDirection = rb2d.velocity;
+		if (moveDirection != Vector2.zero)
+		{
+			float angle = -90 + Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+}
