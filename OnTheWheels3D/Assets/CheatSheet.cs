c68ec380d0d3972507beb92979241db757ba24a5
MaggGomes <magg.slb@gmail.com>
2018-03-02 19:33:29 +0000

    code refactored

---
+++ OnTheWheels3D/Assets/CheatSheet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatSheet : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		print ("collision cheatsheet");
+		if (other.tag == "PlayerTag") {
+			// TODO - fazer update do score de cábulas
+			//other.GetComponent<CarController>.updateScore ();
+
+			Destroy (this.gameObject);
+		}	
+
+		Destroy (this.gameObject);
+	}
+}
