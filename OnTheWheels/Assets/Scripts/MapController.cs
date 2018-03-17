7ed28c3be024b77f7f62c819967f2fbdf7a346dd
Bruno Santos <bruno.santos1996@gmail.com>
2018-03-17 04:40:14 +0000

    controls menu

--- OnTheWheels/Assets/Scripts/MapController.cs
+++ OnTheWheels/Assets/Scripts/MapController.cs
@@ -182,7 +182,7 @@ public class MapController : MonoBehaviour {
 		Cop.GetComponent<CarController>().leftKey = KeyCode.LeftArrow;
 		Cop.GetComponent<CarController>().rightKey = KeyCode.RightArrow;
 		Cop.GetComponent<CarController>().handbrakeKey = KeyCode.RightControl;
-		Cop.GetComponent<CarController>().nitroKey = KeyCode.Space;
+		Cop.GetComponent<CarController>().nitroKey = KeyCode.RightShift;
 		Cop.GetComponent<CarController>().rocketKey = KeyCode.Return;
 		Cop.GetComponent<CarController>().GetComponent<SpriteRenderer>().sprite = policeSprites[currentPoliceSprite];
 		Cop.GetComponent<CarController>().acceleration = policeCar.acceleration;
@@ -209,8 +209,8 @@ public class MapController : MonoBehaviour {
 		Player.GetComponent<CarController>().brakeKey = KeyCode.S;
 		Player.GetComponent<CarController>().leftKey = KeyCode.A;
 		Player.GetComponent<CarController>().rightKey = KeyCode.D;
-		Player.GetComponent<CarController>().handbrakeKey = KeyCode.LeftShift;
-		Player.GetComponent<CarController>().nitroKey = KeyCode.LeftControl;
+		Player.GetComponent<CarController>().handbrakeKey = KeyCode.LeftControl;
+		Player.GetComponent<CarController>().nitroKey = KeyCode.LeftShift;
 		Player.GetComponent<CarController>().rocketKey = KeyCode.Q;
         Player.GetComponent<CarController>().GetComponent<SpriteRenderer>().sprite = selectedCar.sprite;
         Player.GetComponent<CarController>().acceleration = selectedCar.acceleration;
